e2c4f14a3240a1e22ce210ea0b1b027d780c2ba8
Texas Paladin <58018106+TexasPaladin@users.noreply.github.com>
2019-12-03 22:37:14 -0600

    Add project files.

---
+++ RandomNumberGenerator/frmMain.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbxNumbers = New System.Windows.Forms.ListBox()
+        Me.btnRun = New System.Windows.Forms.Button()
+        Me.lblHigh = New System.Windows.Forms.Label()
+        Me.lblLow = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lbxNumbers
+        '
+        Me.lbxNumbers.FormattingEnabled = True
+        Me.lbxNumbers.ItemHeight = 16
+        Me.lbxNumbers.Location = New System.Drawing.Point(72, 43)
+        Me.lbxNumbers.Name = "lbxNumbers"
+        Me.lbxNumbers.Size = New System.Drawing.Size(232, 196)
+        Me.lbxNumbers.TabIndex = 0
+        '
+        'btnRun
+        '
+        Me.btnRun.Location = New System.Drawing.Point(128, 319)
+        Me.btnRun.Name = "btnRun"
+        Me.btnRun.Size = New System.Drawing.Size(75, 23)
+        Me.btnRun.TabIndex = 1
+        Me.btnRun.Text = "Run"
+        Me.btnRun.UseVisualStyleBackColor = True
+        '
+        'lblHigh
+        '
+        Me.lblHigh.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblHigh.Location = New System.Drawing.Point(38, 275)
+        Me.lblHigh.Name = "lblHigh"
+        Me.lblHigh.Size = New System.Drawing.Size(100, 23)
+        Me.lblHigh.TabIndex = 2
+        Me.lblHigh.Text = " "
+        '
+        'lblLow
+        '
+        Me.lblLow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblLow.Location = New System.Drawing.Point(204, 275)
+        Me.lblLow.Name = "lblLow"
+        Me.lblLow.Size = New System.Drawing.Size(100, 23)
+        Me.lblLow.TabIndex = 3
+        Me.lblLow.Text = " "
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(358, 373)
+        Me.Controls.Add(Me.lblLow)
+        Me.Controls.Add(Me.lblHigh)
+        Me.Controls.Add(Me.btnRun)
+        Me.Controls.Add(Me.lbxNumbers)
+        Me.Name = "frmMain"
+        Me.Text = "Random Number Generator"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lbxNumbers As ListBox
+    Friend WithEvents btnRun As Button
+    Friend WithEvents lblHigh As Label
+    Friend WithEvents lblLow As Label
+End Class
---
+++ RandomNumberGenerator/frmMain.vb
@@ -0,0 +1,64 @@
+﻿Public Class frmMain
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+        Dim rnd As New Random
+
+        Dim strNumber As String = String.Empty
+        Dim intNumber As Integer = 0
+        Dim intRoll As Integer = 0
+
+        Dim intSum As Integer = 0
+        Dim decAverage As Decimal = 0.0D
+        Dim intHigh As Integer = 0
+        Dim intLow As Integer = 0
+
+        Dim intCount As Integer = 0
+        Dim bolIsValid As Boolean = False
+        Dim bolFirstTime As Boolean = True
+
+        Try
+            lbxNumbers.Items.Clear()
+
+            For intCount = 1 To 5
+                Do
+                    strNumber = InputBox("Enter a number between 1 and 100", "Number Please - Prompt #" & intCount.ToString, "10")
+                    strNumber = strNumber.Trim
+                    If strNumber = String.Empty Then
+                        Exit For
+                    End If
+                    If Integer.TryParse(strNumber, intNumber) Then
+                        bolIsValid = True
+
+                        intRoll = rnd.Next(1, intNumber) + 1
+                        lbxNumbers.Items.Add(intRoll.ToString)
+                        intSum += intRoll
+                        If intRoll > intHigh Then
+                            intHigh = intRoll
+                        End If
+                        If bolFirstTime Then
+                            intLow = intRoll
+                            bolFirstTime = False
+                        End If
+                        If intLow > intRoll Then
+                            intLow = intRoll
+                        End If
+                    Else
+                        MessageBox.Show("Not an Integer value, please try again.", "Not Integer", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    End If
+                Loop Until bolIsValid
+                bolIsValid = False
+            Next
+            MsgBox("Average: " & (intSum / intCount).ToString("n1"))
+            lblHigh.Text = intHigh.ToString
+            lblLow.Text = intLow.ToString
+        Catch ex As Exception
+            MessageBox.Show("A General Error has occured: " & ControlChars.NewLine & ControlChars.NewLine & ex.Message,
+                            "General Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        'test
+    End Sub
+End Class
